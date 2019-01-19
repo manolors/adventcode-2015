d0bce76b137e59cfd2521ad1b92e0c80ac9ec0cf
= <=>
2019-01-19 00:42:43 +0000

    Day 2 - part 2

--- 2/main.go
+++ 2/main.go
@@ -23,6 +23,22 @@ func getSmallestSide(l int, w int, h int) int {
 	return l * w
 }
 
+func getRibbonSize(l int, w int, h int) int {
+	if l > w && l > h {
+		return w*2 + h*2
+	}
+
+	if w > h {
+		return l*2 + h*2
+	}
+
+	return l*2 + w*2
+}
+
+func getCubicVolume(l int, w int, h int) int {
+	return l * w * h
+}
+
 func lineToDimensions(s string) (int, int, int) {
 	dimensions := strings.Split(s, "x")
 	l, err := strconv.Atoi(dimensions[0])
@@ -48,15 +64,19 @@ func main() {
 	}
 	line := ""
 	totalPaper := 0
+	ribbonSize := 0
 	for index := 0; index <= len(file); index++ {
 		if index == len(file) || file[index] == 10 {
 			l, w, h := lineToDimensions(line)
 			totalPaper = totalPaper + calcDimensions(l, w, h) + getSmallestSide(l, w, h)
+			ribbonSize = ribbonSize + getRibbonSize(l, w, h) + getCubicVolume(l, w, h)
 			line = ""
 		} else {
 			line = line + string(file[index])
 		}
 	}
 
-	fmt.Print("Total Paper: ", totalPaper)
+	fmt.Println("Total Paper: ", totalPaper)
+	fmt.Println("Ribbon Size: ", ribbonSize)
+
 }
