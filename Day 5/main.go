a1866220b19c7aa18246dd763f25fc1bb5fa26d4
= <=>
2019-01-19 13:54:54 +0000

    Day 5 - part 1

---
+++ Day 5/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
+func inc(i *int) {
+	*i = *i + 1
+}
+func checkNiceVowels(s string) bool {
+	matches := 0
+
+	for _, rune := range s {
+		if strings.ContainsAny(string(rune), "a & e & i & o & u") {
+			inc(&matches)
+		}
+	}
+	return matches >= 3
+}
+
+func checkDuplicateLetter(s string) bool {
+	prevRune := s[0]
+	for index := 1; index < len(s); index++ {
+		if s[index] == prevRune {
+			return true
+		}
+		prevRune = s[index]
+	}
+	return false
+}
+func checkBlacklist(s string) bool {
+	blacklist := []string{"ab", "cd", "pq", "xy"}
+	for _, rune := range blacklist {
+		if strings.Contains(s, rune) {
+			return false
+		}
+	}
+	return true
+}
+
+func isNiceString(s string) bool {
+	checkVowels := checkNiceVowels(s)
+	checkDupes := checkDuplicateLetter(s)
+	checkBlack := checkBlacklist(s)
+	return checkBlack && checkDupes && checkVowels
+}
+
+func main() {
+	file, err := ioutil.ReadFile("input.txt")
+
+	if err != nil {
+		panic(err)
+	}
+
+	line := ""
+	nice := 0
+	for index := 0; index <= len(file); index++ {
+		if index == len(file) || file[index] == 10 {
+			if isNiceString(line) {
+				inc(&nice)
+			}
+			line = ""
+		} else {
+			line = line + string(file[index])
+		}
+	}
+
+	fmt.Println("Nice strings:", nice)
+}
