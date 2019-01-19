21fdaf02a3e6f91498b2a0c089d3daeb75f7e0e4
= <=>
2019-01-19 01:20:53 +0000

    Day 3 - Part 2

--- 3/main.go
+++ 3/main.go
@@ -6,8 +6,22 @@ import (
 	"strconv"
 )
 
-type Coords struct {
-	x, y int
+func getMapIndex(dir string, x *int, y *int) string {
+	switch dir {
+	case "<":
+		*x = *x - 1
+		break
+	case ">":
+		*x = *x + 1
+		break
+	case "^":
+		*y = *y + 1
+		break
+	case "v":
+		*y = *y - 1
+		break
+	}
+	return strconv.Itoa(*x) + "," + strconv.Itoa(*y)
 }
 
 func main() {
@@ -17,28 +31,19 @@ func main() {
 		panic(err)
 	}
 
-	x, y := 0, 0
+	xSanta, ySanta, xRobosanta, yRobosanta := 0, 0, 0, 0
+
 	m := make(map[string]int)
 	m["0,0"] = 1
+	isSantaTurn := true
+	mapIndex := ""
 	for _, direction := range file {
-		dir := string(direction)
-
-		switch dir {
-		case "<":
-			x = x - 1
-			break
-		case ">":
-			x = x + 1
-			break
-		case "^":
-			y = y + 1
-			break
-		case "v":
-			y = y - 1
-			break
+		if isSantaTurn {
+			mapIndex = getMapIndex(string(direction), &xSanta, &ySanta)
+		} else {
+			mapIndex = getMapIndex(string(direction), &xRobosanta, &yRobosanta)
 		}
-
-		mapIndex := strconv.Itoa(x) + "," + strconv.Itoa(y)
+		isSantaTurn = !isSantaTurn
 
 		val, ok := m[mapIndex]
 		if ok {
