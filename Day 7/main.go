46c03ce39a159d2b990c165f32d5b1b6ce5b3d46
manolors <manolo.rs2005@gmail.com>
2019-01-20 19:59:06 +0000

    Day 7

--- Day 7/main.go
+++ Day 7/main.go
@@ -6,32 +6,47 @@ import (
 	"strings"
 )
 
-var wires map[string]int
+var wires map[string]uint16
 
 func getData(s string) {
+	wires := make(map[string]uint16)
+
 	data := strings.Split(s, " -> ")
 	destinationWire := data[1]
 	wires[destinationWire] = 0
 
+	data = strings.Split(data[0], " ")
+
 	signal, err := strconv.Atoi(data[0])
 
 	if err != nil {
-		data := strings.Split(s, " ")
-
 		if len(data) == 2 {
-			operation := "NOT"
-			operand1 := data[1]
+			wires[destinationWire] ^= wires[data[1]]
 		} else {
-			operand1 := data[0]
-			operation := data[1]
-			operand2 := data[2]
+			wire1, _ := wires[data[0]]
+			wire2, _ := wires[data[2]]
 
+			switch data[1] {
+			case "AND":
+				wires[destinationWire] = wire1 & wire2
+				break
+			case "OR":
+				wires[destinationWire] = wire1 | wire2
+				break
+			case "LSHIFT":
+				wires[destinationWire] = wire1 << wire2
+				break
+			case "RSHIFT":
+				wires[destinationWire] = wire1 >> wire2
+				break
+			}
 		}
+	} else {
+		wires[data[1]] = uint16(signal)
 	}
 }
 
 func main() {
-	wires := make(map[string]int)
 	file, err := ioutil.ReadFile("input.txt")
 
 	if err != nil {
